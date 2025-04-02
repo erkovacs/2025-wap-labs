b04d0d688e00e6cee31f811872e52bf103277f40
erik <kovaroberik@gmail.com>
2025-04-02 21:32:28 +0300

    Added seminar 5

---
+++ Seminar5/Seminar5/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar4.Entities
+{
+    internal class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName {
+            get => $"{Name} {Surname}";
+        }
+        public int Year { get; set; }
+    }
+}
